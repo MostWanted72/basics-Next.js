1c999e1aaf9aa32cf533ddd4326bad371b0fd037
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-09-22 19:18:09 +0530

    image in next

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+import homeImg from "@/../public/home.jpg";
 
 export default function Home() {
   return (
     <div>
       <div>Home Page</div>
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="car factory"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
     </div>
   );
 }
